8e048e5e36d915b280f6e2587abe09a7e8b9cf88
Ahmed Musa <50640701+mu-ab@users.noreply.github.com>
2023-08-17 18:36:06 +0100

    Update LayoutWrapper.tsx

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google'
 import SectionContainer from './SectionContainer'
-import Footer from './Footer'
+// import Footer from './Footer'
 import { ReactNode } from 'react'
 import Header from './Header'
 
@@ -18,7 +18,7 @@ const LayoutWrapper = ({ children }: Props) => {
       <div className={`${inter.className} flex h-screen flex-col justify-between font-sans`}>
         <Header />
         <main className="mb-auto">{children}</main>
-        <Footer />
+{/*         <Footer /> */}
       </div>
     </SectionContainer>
   )

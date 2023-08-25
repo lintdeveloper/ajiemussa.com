24185ee750b5e599c8934a9458e5b812aa7d300d
Ahmed Musa <eckkoenterprise@gmail.com>
2023-08-25 23:59:02 +0100

    extracted pagination into it's own component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,65 +1,18 @@
-'use client'
+// 'use client'
 // import ListLayoutWithPagination from './ListLayoutWithPagination'
 import { allCoreContent, sortPosts, CoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
 
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-
 import BlogCard from '@/components/BlogList'
 import Image from 'next/image'
+import Pagination from '@/components/Pagination'
 
 import formatDateToCustomFormat from '../../scripts/formatDate'
 
 const POSTS_PER_PAGE = 5
 
 export const metadata = genPageMetadata({ title: 'Blog' })
-
-interface PaginationProps {
-  totalPages: number
-  currentPage: number
-}
-
-function Pagination({ totalPages, currentPage }: PaginationProps) {
-  const pathname = usePathname()
-  const basePath = pathname.split('/')[1]
-  const prevPage = currentPage - 1 > 0
-  const nextPage = currentPage + 1 <= totalPages
-
-  return (
-    <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-      <nav className="flex justify-between">
-        {!prevPage && (
-          <button className="cursor-auto disabled:opacity-50" disabled={!prevPage}>
-            Previous
-          </button>
-        )}
-        {prevPage && (
-          <Link
-            href={currentPage - 1 === 1 ? `/${basePath}/` : `/${basePath}/page/${currentPage - 1}`}
-            rel="prev"
-          >
-            Previous
-          </Link>
-        )}
-        <span>
-          {currentPage} of {totalPages}
-        </span>
-        {!nextPage && (
-          <button className="cursor-auto disabled:opacity-50" disabled={!nextPage}>
-            Next
-          </button>
-        )}
-        {nextPage && (
-          <Link href={`/${basePath}/page/${currentPage + 1}`} rel="next">
-            Next
-          </Link>
-        )}
-      </nav>
-    </div>
-  )
-}
 
 export default function BlogPage() {
   const posts = allCoreContent(sortPosts(allBlogs))
